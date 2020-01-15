691cde237d7e8026f5510475d68e481bdd315fac
unknown <muhdhafi8160@gmail.com>
2020-01-15 18:43:01 +0800

    added-viewlist

--- src/app/customer/customer.page.ts
+++ src/app/customer/customer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostProvider } from '../../providers/post-provider';
 import { Router } from '@angular/router'; 
 
 @Component({
@@ -7,13 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./customer.page.scss'],
 })
 export class CustomerPage implements OnInit {
+  customers: any = [];
+  limit: number = 10;
+  start: number = 0;
 
-  constructor(private router: Router) { }
+  constructor(private postprovider: PostProvider, private router: Router) { }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter(){
+    this.customers = [];
+    this.start = 0;
+    this.loadCustomer();
+  }
+
   addCustomer(){
     this.router.navigate(['/addcustomer']);
+  }
+
+  loadCustomer(){
+    return new Promise(resolve => {
+      let body = {
+        action : 'getdata',
+        limit : this.limit,
+        start : this.start,
+      };
+
+      this.postprovider.postData(body, 'process-api.php').subscribe(data => {
+        for(let customer of data.result){
+          this.customers.push(customer);
+        }
+        resolve(true);
+      });
+    });
   }
 }
