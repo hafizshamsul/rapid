6cb0fe4f43902df875e25b6c846552884ffd392d
unknown <muhdhafi8160@gmail.com>
2020-02-14 21:27:25 +0800

    added-threadfilter

--- src/app/thread/thread.page.ts
+++ src/app/thread/thread.page.ts
@@ -1,99 +1,215 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { PostProvider } from '../../providers/post-provider';
 import { Router, ActivatedRoute } from '@angular/router';
-import { GlobalService } from "../../providers/global.service";
+import { AlertController } from '@ionic/angular';
+import { ImagesProvider } from '../../providers/images/images';
+import { HttpClient } from "@angular/common/http";
+import { GlobalService } from "../..//providers/global.service";
+import { IonicPage, Item } from 'ionic-angular';
+import {AppRoutingModule} from '../app-routing.module';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-thread',
-  templateUrl: './thread.page.html',
-  styleUrls: ['./thread.page.scss'],
+  templateUrl: 'thread.page.html',
+  styleUrls: ['thread.page.scss'],
 })
-export class ThreadPage implements OnInit {
-  name_customer: string = "";
-  desc_customer: string = "";
-  id: number;
 
-  current_userid: number;
-  current_username: string;
-  current_passwordhash: string;
-  current_displayname: string;
 
-  folderid: number;
-  foldername: string;
-  description: string;
+export class ThreadPage {
+
+  constructor(
+    public navCtrl: NavController,
+    public route: AppRoutingModule,
+    public global: GlobalService, 
+    private actRoute: ActivatedRoute,
+    public alertCtrl: AlertController, private postprovider: PostProvider, private router: Router, private _IMAGES: ImagesProvider, private http: HttpClient) {
+    
+      
+    }
+    //commentid: number;
+    //replyto: number;
+
+    listo:any[]=[
+      {
+        commentid: 1,
+        title: '1',
+        textcmt: 'satu',
+        users_id: 1,
+        username: 'hafizshamsul',
+        dateuploaded: 'days ago',
+        vote: 213,
+        replyto: null
+      },
+      {
+        commentid: 2,
+        title: '2',
+        textcmt: 'dua',
+        users_id: 1,
+        username: 'hafizshamsul',
+        dateuploaded: 'days ago',
+        vote: 213,
+        replyto: 1
+      },
+      {
+        commentid: 3,
+        title: '3',
+        textcmt: 'tiga',
+        users_id: 1,
+        username: 'hafizshamsul',
+        dateuploaded: 'days ago',
+        vote: 213,
+        replyto: 1
+      },
+      {
+        commentid: 4,
+        title: '4',
+        textcmt: 'empat',
+        users_id: 1,
+        username: 'hafizshamsul',
+        dateuploaded: 'days ago',
+        vote: 213,
+        replyto: 2
+      }
+    ];
+
+    
+
+    
+
+
+ list:any = [
+        {id:1, title:"1", parent:null,
+            children:[
+                    {id:2, title:"2", parent:1, children:[]}
+                 ],
+                }
+    ];
+    
+  current_users: any[];
+  comments: any[];
+
+  treeify(listo, idAttr, parentAttr, childrenAttr) {
+    if (!idAttr) idAttr = 'commentid';
+    if (!parentAttr) parentAttr = 'replyto';
+
+    if (!childrenAttr) childrenAttr = 'children';
+
+    var treeList = [];
+    var lookup = {};
+    listo.forEach(function(obj) {
+        lookup[obj[idAttr]] = obj;
+        obj[childrenAttr] = [];
+    });
+    listo.forEach(function(obj) {
+        if (obj[parentAttr] != null) {
+            lookup[obj[parentAttr]][childrenAttr].push(obj);
+        } else {
+            treeList.push(obj);
+        }
+      });
+      return treeList;
+  };
+
+
+listos:any[] = this.treeify(this.listo, 'commentid', 'replyto', 'children');
+listoso:any[]; 
+
+
+  tagcomments: any[];
+
+  done: boolean =true;
+
+  commentid: number;
+  title: string;
+  textcmt: string;
   users_id: number;
+  replyto: number;
+  username: string;
+  dateuploaded: string;
+  vote: number;
 
-  constructor(public global: GlobalService, private postprovider: PostProvider, private router: Router, private actRoute: ActivatedRoute) { }
+  tagcommentid: number;
+  comment_id: number;
+  tag_id: number;
+  tag_tagname: string;
+
+  pass(index){
+    console.log(index);
+  }
+
 
   ngOnInit() {
-    this.actRoute.params.subscribe((data: any) =>{
-      this.current_userid = data.current_id;
-      this.current_username = data.current_username;
-      this.current_passwordhash = data.current_passwordhash;
-      this.current_displayname = data.current_displayname;
-      console.log(data);
-    });
-    
-    this.actRoute.params.subscribe((data: any) =>{
-      this.id = data.id;
-      this.name_customer = data.name;
-      this.desc_customer = data.desc;
-      console.log(data);
-    });
+    console.log(JSON.stringify(this.list));
+    console.log(JSON.stringify(this.listos));
 
     this.actRoute.params.subscribe((data: any) =>{
-      this.folderid = data.folderid;
-      this.foldername = data.foldername;
-      this.description = data.description;
+      this.commentid = data.commentid;
       this.users_id = data.users_id;
+      this.title = data.title;
+      this.textcmt = data.textcmt;
+      this.replyto = data.replyto;
+      this.username = data.username;
+      this.dateuploaded = data.dateuploaded;
+      this.vote = data.vote;
+
+      console.log(data);
+    });
+
+    this.actRoute.params.subscribe((data: any) =>{
+      this.tagcommentid = data.tagcommentid;
+      this.comment_id = data.comment_id;
+      this.tag_id = data.tag_id;
+      this.tag_tagname = data.tag_tagname;
+
       console.log(data);
     });
   }
 
-  createdProcess(){
+  ionViewWillEnter(){
+    this.comments = [];
+    this.tagcomments = [];
+    this.loadThread();
+    this.loadTagComment(1);
+  
+  }
+
+  login(){
+    this.router.navigate(['/loginform']);
+  }
+
+
+
+  loadThread(){
     return new Promise(resolve => {
       let body = {
-        action: 'add',
-        name_customer: this.name_customer,
-        desc_customer: this.desc_customer,
+        action : 'getthread',
       };
-
-      this.postprovider.postData(body, 'process-api.php').subscribe(data=>{
-        this.router.navigate(['/customer']);
-        console.log('OK');
+      this.postprovider.postData(body, 'process-api.php').subscribe(data => {
+        for(let comment of data.result){
+          this.comments.push(comment);
+        }
+        this.listoso = this.treeify(this.comments, 'commentid', 'replyto', 'children');
+        resolve(true);
       });
     });
   }
 
-  createdFolder(){
+  loadTagComment(index){
     return new Promise(resolve => {
       let body = {
-        action: 'addfolder',
-        foldername: this.foldername,
-        description: this.description,
-        users_id: this.global.userid,
+        action : 'gettagcomment',
+        index : index
       };
-
-      this.postprovider.postData(body, 'process-api.php').subscribe(data=>{
-        this.router.navigate(['/'+this.global.username]);
-        console.log('OK');
+      this.postprovider.postData(body, 'process-api.php').subscribe(data => {
+        for(let tagcomment of data.result){
+          //if(tagcomment.comment_id == 10){
+            this.tagcomments.push(tagcomment);
+          //}
+        }
+        resolve(true);
       });
     });
   }
-
-  updatedProcess(){
-    return new Promise(resolve => {
-      let body = {
-        action: 'update',
-        customer_id: this.id,
-        name_customer: this.name_customer,
-        desc_customer: this.desc_customer,
-      };
-
-      this.postprovider.postData(body, 'process-api.php').subscribe(data=>{
-        this.router.navigate(['/customer']);
-        console.log('OK');
-      });
-    });
-  }
+  
 }
