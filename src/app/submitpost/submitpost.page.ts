6937cd48a30c183208c6635f280d3e435b27fe5e
unknown <muhdhafi8160@gmail.com>
2020-03-25 06:55:25 +0800

    added-quillview

--- src/app/submitpost/submitpost.page.ts
+++ src/app/submitpost/submitpost.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 
 @Component({
   selector: 'app-submitpost',
@@ -8,9 +10,38 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class SubmitpostPage implements OnInit {
 
-  constructor(private router: Router, private actRoute: ActivatedRoute) { }
+  constructor(private qull: QuillModule, private router: Router, private actRoute: ActivatedRoute) { }
+  
+  editorForm: FormGroup;
+  editorContent: string;
+
+  editorStyle = {
+    height: '380px',
+    backgroundColor: 'black'
+  }
+
+  config = {
+    toolbar: [
+      ['bold', 'italic', 'underline'],
+      ['code-block']
+    ]
+  }
 
   ngOnInit() {
+    this.editorForm = new FormGroup({
+      'editor': new FormControl(null)
+    });
+  }
+
+  onSubmit(){
+    this.editorContent = this.editorForm.get('editor').value;
+    console.log(this.editorForm.get('editor').value);
+  }
+
+  maxLength(e){
+    if(e.editor.getLength() > 1000){
+      e.editor.deleteText(10, e.editor.getLength());
+    }
   }
 
 }
